3d5ec6c04976e1ede77bfe49e16efec8dc7555ef
Manuel Alejandro Ortega <morteguita@gmail.com>
2024-04-23 16:18:00 -0500

    Characters endpoints, CRUD and custom filters

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import apiAuth from './api/auth.js';
+import apiCharacters from './api/character.js';
 
 const apiRouter = Router();
 
 apiRouter.use('/auth', apiAuth);
+apiRouter.use('/characters', apiCharacters);
 
 export { apiRouter };
