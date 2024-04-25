de28490ce64070ae64e9fd481887f309a9c18be2
Manuel Alejandro Ortega <morteguita@gmail.com>
2024-04-25 14:51:34 -0500

    Genre getById endpoint with media

--- backend/routes/api/genre.js
+++ backend/routes/api/genre.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getAll } from '../../controllers/genre.js';
+import { getAll, getById } from '../../controllers/genre.js';
 import validateJWT from '../../middlewares/validate_jwt.js';
 
 const apiGenre = Router();
 
 apiGenre.get('/', [validateJWT], getAll);
+apiGenre.get('/:id', [validateJWT], getById);
 
 export default apiGenre;
