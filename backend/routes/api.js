ff7e0f88909c2175e4682fa9f1f5d90c3439012d
Manuel Alejandro Ortega <morteguita@gmail.com>
2024-04-25 08:53:53 -0500

    Genre getAll endpoint for custom filtering

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import apiAuth from './api/auth.js';
 import apiCharacters from './api/character.js';
 import apiMedia from './api/media.js';
+import apiGenre from './api/genre.js';
 
 const apiRouter = Router();
 
 apiRouter.use('/auth', apiAuth);
 apiRouter.use('/characters', apiCharacters);
 apiRouter.use('/movies', apiMedia);
+apiRouter.use('/genres', apiGenre);
 
 export { apiRouter };
